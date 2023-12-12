0785beb18f86dee065152bf0e831f788e24d245c
shayda-banihashemi <shayda.dev@icloud.com>
2023-12-12 12:31:16 -0700

    changed main page

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+AUTHOR = 'Shayda Banihashemi'
+SITENAME = "Shayda's Blog"
+SITEURL = "https://shayda-banihashemi.github.io"
+DOMAIN = SITEURL
+FEED_DOMAIN = SITEURL
+HTTPS = True
+
+PATH = "content"
+
+TIMEZONE = 'America/Phoenix'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+SOCIAL = (
+    ("GitHub", "https://github.com/shayda-banihashemi"),
+    ("LinkedIn", "https://www.linkedin.com/in/shayda-banihashemi"),
+ )
+
+# Social widget
+LINKS = (
+    ("GitHub", "https://github.com/shayda-banihashemi"),
+    ("LinkedIn", "https://www.linkedin.com/in/shayda-banihashemi"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
