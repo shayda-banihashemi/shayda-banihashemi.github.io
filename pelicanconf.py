b1923987efd3ac3a56c8fc148a7a560c810669d9
shayda-banihashemi <shayda.dev@icloud.com>
2023-12-12 12:19:07 -0700

    modified links

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+AUTHOR = 'Shayda Banihashemi'
+SITENAME = "Shayda's Blog"
+SITEURL = "https://shayda-banihashemi.github.io"
+DOMAIN = SITEURL
+FEED_DOMAIN = SITEURL
+HTTPS = True
+
+PATH = "content"
+
+TIMEZONE = 'America/Phoenix'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("GitHub", "github.com/shayda-banihashemi"),
+    ("LinkedIn", "www.linkedin.com/in/shayda-banihashemi"),
+    ("You can modify those links in your config file", "#"),
+ )
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
